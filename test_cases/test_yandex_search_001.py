6bbeddda4b74ac1e340d0e5da9601dbb1ab76bd1
mecher <maximchernyak98@gmail.com>
2020-12-15 12:36:18 +0300

    minor

--- test_cases/test_yandex_search_001.py
+++ test_cases/test_yandex_search_001.py
@@ -11,28 +11,29 @@ from page_objects.yandex_page import YandexSearchPage
 
 class Test_001_Search:
     logger = LogGen.loggen()
-    logger.info("***Test_001_Search***")
+    # TODO сделать логгер в режиме добавления записей
+    logger.info("Test_001_Search")
     path_to_screenshots = os.path.join('.', 'screenshots')
     # TODO добавить скриншоты
 
     yandexURL = ReadConfig.get_application_url()
 
     def test_check_search_box(self, setup):
-        self.logger.info("***Checking if a search field exists***")
+        self.logger.info("Checking if a search field exists")
         self.driver = setup
         self.driver.get(self.yandexURL)
         self.yp = YandexSearchPage(self.driver)
         try:
             self.yp.find_search_box()
-            self.logger.info(f"***Test passed: Search field found by {self.yp.search_box_locator}***")
+            self.logger.info(f"Test passed: Search field found by {self.yp.search_box_locator}")
         except TimeoutException:
-            self.logger.error(f"***Test failed: Search field not found by {self.yp.search_box_locator} for {self.yp.delay} seconds***")
+            self.logger.error(f"Test failed: Search field not found by {self.yp.search_box_locator} for {self.yp.delay} seconds")
         finally:
             self.driver.close()
 
     @pytest.mark.current
     def test_suggests_in_search_box(self, setup):
-        self.logger.info("***Checking suggests in search field***")
+        self.logger.info("Checking suggests in search field")
         self.driver = setup
         self.driver.get(self.yandexURL)
         self.yp = YandexSearchPage(self.driver)
@@ -46,10 +47,10 @@ class Test_001_Search:
                                                   link_to_search=r"https://tensor.ru/",
                                                   number_results=5)
             else:
-                self.logger.error("***Test failed: There is no links in result of search***")
+                self.logger.error("Test failed: There is no links in result of search")
         except TimeoutException:
             # TODO добавить отработку различных ненайденок
-            self.logger.error(f"***Test failed: Suggest field not found by {self.yp.suggest_box_locator} for {self.yp.delay} seconds***")
+            self.logger.error(f"Test failed: Suggest field not found by {self.yp.suggest_box_locator} for {self.yp.delay} seconds")
         finally:
             self.driver.close()
 
@@ -63,8 +64,8 @@ class Test_001_Search:
         if link_to_search in list_of_links:
             index = list_of_links.index(link_to_search) + 1
             if index <= number_results:
-                self.logger.info(f"***Test passed: {link_to_search} find in {index} request position***")
+                self.logger.info(f"Test passed: {link_to_search} find in {index} request position")
             else:
-                self.logger.error(f"***Test failed: {link_to_search} find, but in {index} request position***")
+                self.logger.error(f"Test failed: {link_to_search} find, but in {index} request position")
         else:
-            self.logger.error(f"***Test failed: {link_to_search} did not find in request***")
+            self.logger.error(f"Test failed: {link_to_search} did not find in request")
